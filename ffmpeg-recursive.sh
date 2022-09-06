9237a3c711a2b0906f189d207996b73d0beddece
eye <101060047+px585px@users.noreply.github.com>
2022-09-06 23:15:37 +0200

    Update ffmpeg-recursive.sh

--- ffmpeg-recursive.sh
+++ ffmpeg-recursive.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
+#changing the $IFS variable that determine what the field separators are to not use space
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+
 #take folder name from user and cchange directory to it if it exists
 [[ -z $# ]] && dir=$(pwd) || dir=$1
 cd $dir || exit
-echo $(pwd)
 
 #take some input from the user
 read -p "Enter input filetype (leave empty to select all): " iext
 read -p "Enter output filetype (required): " oext 
-[[ -z $oext ]] && echo "You can't leave it empty"; exit
+[[ -z $oext ]] && echo "You can't leave it empty" && exit
 read -p "Enter ffmpeg flags to use (optinal): " flags
 
 #loop through folders and execute ffmpeg on files whith matching input extention
-for file in $(find . -name "*$iext"); do
+for file in $(find . -type f -name "*$iext"); do
     echo $file
-    echo "${file%.*}"
     $(ffmpeg -i $file $flags ${file%.*}.$oext)
 done
+
+#restoring default $IFS value
+IFS=$SAVEIFS
